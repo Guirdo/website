b59c75f3e33105327fd8bdfbe4e818613699e147
Guirdo <produccionesaldo@gmail.com>
2023-02-25 10:21:53 -0600

    🔧 Refactor filters and collections configuration * 🔧 Add array filters * 🔧 Update projects collections * ➕ Add fast-glob dependency

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,27 @@
-const { getAllProjects, getPromotedProjects } = require('./config/collections/projects.js');
+const glob = require('fast-glob')
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('./src/assets');
+  /* -------- Collection --------  */
+  glob.sync('./config/collections/*.js').forEach((file) => {
+    let collection = require('./' + file);
+    Object.keys(collection).forEach((name) => {
+      eleventyConfig.addCollection(name, collection[name]);
+    });
+  });
 
-  eleventyConfig.addCollection('projects', getAllProjects)
-  eleventyConfig.addCollection('promotedProjects', getPromotedProjects)
-  
+  /* -------- Filters --------  */
+  glob.sync('./config/filters/*.js').forEach((file) => {
+    let filters = require('./' + file);
+    Object.keys(filters).forEach((name) => {
+      eleventyConfig.addFilter(name, filters[name]);
+    });
+  });
+
   eleventyConfig.addPlugin(require('./config/template-languages/css-config.js'))
   eleventyConfig.addPlugin(require('./config/template-languages/js-config.js'))
 
+  eleventyConfig.addWatchTarget('./src/assets');
+  
   eleventyConfig.addPassthroughCopy('src/assets/fonts/')
   eleventyConfig.addPassthroughCopy({
     './src/assets/images/favicon/*': '/'
--- config/collections/projects.js
+++ config/collections/projects.js
@@ -1,16 +1,12 @@
-const getAllProjects = collection => {
-  return collection
-      .getFilteredByGlob('./src/projects/*.md')
-      .reverse()
-}
-
-const getPromotedProjects = collection => {
-  return collection
-      .getFilteredByGlob('./src/projects/*.md')
-      .filter(item => item.data.promoted)
-}
-
 module.exports = {
-  getAllProjects,
-  getPromotedProjects
+  projects: (collection) => {
+    return collection
+      .getFilteredByGlob('./src/projects/**/*.md')
+      .reverse()
+  },
+  promotedProjects: (collection) => {
+    return collection
+        .getFilteredByGlob('./src/projects/**/*.md')
+        .filter(item => item.data.promoted)
+  }
 }
