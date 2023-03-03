67c94f0184a9871fe3ff2b9bef5fdb0bdaaa82c5
Guirdo <produccionesaldo@gmail.com>
2023-03-03 12:58:53 -0600

    🌐  Add project layout translation * 🌐 Refactor common translations * 💬 Fix literals for i18n

--- src/_data/i18n/index.js
+++ src/_data/i18n/index.js
@@ -1,41 +1,13 @@
 const themes = require('./themes.json')
 const hero = require('./hero.json')
 const social = require('./social.json')
+const project = require('./project.json')
+const common = require('./common.json')
 
 module.exports = {
   themes,
   hero,
   social,
-  es: {
-    'es':'Español',
-    'en':'Spanish'
-  },
-  en: {
-    'es':'Inglés',
-    'en':'English'
-  },
-  madeWith: {
-    'es':'Hecho con ❤️ por {{ name }}',
-    'en':'Made with ❤️ by {{ name }}',
-  },
-  currentLang: {
-    'es':'Idioma actual',
-    'en':'Current language'
-  },
-  about: {
-    'es':'Sobre mi',
-    'en':'About me'
-  },
-  projects: {
-    'es':'Proyectos',
-    'en':'Projects'
-  },
-  contact: {
-    'es':'Contacto',
-    'en':'Contact'
-  },
-  translateToLang: {
-    'es':'Traducir al',
-    'en':'Translate to'
-  }
+  project,
+  common
 }
