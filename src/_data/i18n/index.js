9c28e56f3cc2a594a06c8e201b70a8f1bae12805
Guirdo <produccionesaldo@gmail.com>
2023-03-05 15:01:37 -0600

    🌐 Add archive translations * 🎨 Sort objects and their imports alphabetically

--- src/_data/i18n/index.js
+++ src/_data/i18n/index.js
@@ -1,13 +1,15 @@
-const themes = require('./themes.json')
-const hero = require('./hero.json')
-const social = require('./social.json')
-const project = require('./project.json')
+const archive = require('./archive.json')
 const common = require('./common.json')
+const hero = require('./hero.json')
+const project = require('./project.json')
+const social = require('./social.json')
+const themes = require('./themes.json')
 
 module.exports = {
-  themes,
+  archive,
+  common,
   hero,
-  social,
   project,
-  common
+  social,
+  themes
 }
